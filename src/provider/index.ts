186ebb243bc798261380d1fcd47ab20e53cb1613
saninsteinn <saninsteinn@gmail.com>
2021-11-26 10:11:19 +0200

    removed HttpProvider options

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -12,6 +12,6 @@ const websocketOptions = {
 export const web3 = new Web3(
     process.env.ETHEREUM_WEBSOCKET_URL.startsWith('ws') ?
     new Web3.providers.WebsocketProvider(process.env.ETHEREUM_WEBSOCKET_URL, websocketOptions) :
-    new Web3.providers.HttpProvider(process.env.ETHEREUM_WEBSOCKET_URL, websocketOptions)
+    new Web3.providers.HttpProvider(process.env.ETHEREUM_WEBSOCKET_URL)
 )
 export const web3Proof = process.env.PROOFS_RPC_URL ? new Web3(process.env.PROOFS_RPC_URL) : web3
